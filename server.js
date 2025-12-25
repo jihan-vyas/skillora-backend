11a0504ff7cc6fb2ade3f0a5c24a952a33b394e2
jihan-vyas <vyasjihan2005@gmail.com>
2025-12-25 22:33:04 +0530

    xyz

--- server.js
+++ server.js
@@ -23,31 +23,3 @@ mongoose
   .catch((err) => console.log(err));
 
 
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import courseRoutes from "./routes/courseRoutes.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// // ROOT ROUTE (FIXES Cannot GET /)
-// app.get("/", (req, res) => {
-//   res.send("Server is running 🚀");
-// });
-
-// // API ROUTES
-// app.use("/api/courses", courseRoutes);
-
-// // MONGODB CONNECTION (NO app.listen)
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch(err => console.log(err));
-
-// // EXPORT APP FOR VERCEL
-// export default app;
